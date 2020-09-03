8adde0a0d5cd0ac392642f9452fe06948069a767
wslee950920 <wslee950920@gmail.com>
2020-09-03 13:03:00 +0900

    service worker register

---
+++ src/service-worker.js
@@ -0,0 +1,18 @@
+workbox.setConfig({
+  debug: true,
+});
+
+workbox.precaching.precacheAndRoute(self.__precacheManifest);
+
+workbox.routing.registerRoute(
+  new RegExp("http://d8quat51w5b08.cloudfront.net/resized/*"),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: "s3-images",
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 60,
+        maxAgeSeconds: 365 * 24 * 60 * 60,
+      }),
+    ],
+  })
+);
