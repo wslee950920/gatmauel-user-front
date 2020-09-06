55ccac067c29a0f36afef67ec9dc705451048e63
wslee950920 <wslee950920@gmail.com>
2020-09-06 18:26:10 +0900

    아이콘 추가

--- src/service-worker.js
+++ src/service-worker.js
@@ -5,19 +5,6 @@ workbox.setConfig({
 workbox.precaching.precacheAndRoute(self.__precacheManifest);
 
 workbox.routing.registerRoute(
-  new RegExp("http://d8quat51w5b08.cloudfront.net/resized/*"),
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: "s3-images",
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxEntries: 60,
-        maxAgeSeconds: 365 * 24 * 60 * 60,
-      }),
-    ],
-  })
-);
-
-workbox.routing.registerRoute(
   new RegExp("images/*"),
   new workbox.strategies.CacheFirst({
     cacheName: "public-images",
