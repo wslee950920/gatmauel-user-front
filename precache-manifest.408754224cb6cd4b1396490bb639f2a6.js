89d77abff1412c434f25a5d7907c08bbd5e50fa6
wslee950920 <wslee950920@gmail.com>
2020-09-29 12:50:36 +0900

    Updates

---
+++ precache-manifest.408754224cb6cd4b1396490bb639f2a6.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a58e15e21649437e551873059335c233",
+    "url": "/gatmauel-user-front/index.html"
+  },
+  {
+    "revision": "792b192c7ff4b17e0633",
+    "url": "/gatmauel-user-front/static/css/5.9359fd7b.chunk.css"
+  },
+  {
+    "revision": "3e2158c1d51ada1a5c4f",
+    "url": "/gatmauel-user-front/static/css/main.414c52c5.chunk.css"
+  },
+  {
+    "revision": "eeb7bb437c03aed56497",
+    "url": "/gatmauel-user-front/static/js/0.c33c036f.chunk.js"
+  },
+  {
+    "revision": "17c63afe57185d344ac7",
+    "url": "/gatmauel-user-front/static/js/1.a216034b.chunk.js"
+  },
+  {
+    "revision": "8c65f3ec8d943fef7e18",
+    "url": "/gatmauel-user-front/static/js/10.b4878402.chunk.js"
+  },
+  {
+    "revision": "b96e336647bbc8aaf9af",
+    "url": "/gatmauel-user-front/static/js/11.6d7e5514.chunk.js"
+  },
+  {
+    "revision": "d308805ea4d90365b026",
+    "url": "/gatmauel-user-front/static/js/12.b50df0df.chunk.js"
+  },
+  {
+    "revision": "ef49fe9b36b4ef27e2ef",
+    "url": "/gatmauel-user-front/static/js/13.e617901b.chunk.js"
+  },
+  {
+    "revision": "84c9da34c4e9ef1e6ec1",
+    "url": "/gatmauel-user-front/static/js/2.4563f9b3.chunk.js"
+  },
+  {
+    "revision": "792b192c7ff4b17e0633",
+    "url": "/gatmauel-user-front/static/js/5.46747e97.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/gatmauel-user-front/static/js/5.46747e97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2289ec3f960576023086",
+    "url": "/gatmauel-user-front/static/js/6.90affb73.chunk.js"
+  },
+  {
+    "revision": "fc8e22164e64fe0de7bf",
+    "url": "/gatmauel-user-front/static/js/7.0086d89a.chunk.js"
+  },
+  {
+    "revision": "7e50ca525e9116d09542",
+    "url": "/gatmauel-user-front/static/js/8.0ebe090c.chunk.js"
+  },
+  {
+    "revision": "2b6562e0792c8af2b590",
+    "url": "/gatmauel-user-front/static/js/9.891bada8.chunk.js"
+  },
+  {
+    "revision": "3e2158c1d51ada1a5c4f",
+    "url": "/gatmauel-user-front/static/js/main.94ca1505.chunk.js"
+  },
+  {
+    "revision": "e75ed0bf5bb56b337b56",
+    "url": "/gatmauel-user-front/static/js/runtime-main.413976ea.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/gatmauel-user-front/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/gatmauel-user-front/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/gatmauel-user-front/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/gatmauel-user-front/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/gatmauel-user-front/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/gatmauel-user-front/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/gatmauel-user-front/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/gatmauel-user-front/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/gatmauel-user-front/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/gatmauel-user-front/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/gatmauel-user-front/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/gatmauel-user-front/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/gatmauel-user-front/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/gatmauel-user-front/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/gatmauel-user-front/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/gatmauel-user-front/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/gatmauel-user-front/static/media/fa-solid-900.ee09ad75.woff"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+importScripts("/gatmauel-user-front/precache-manifest.408754224cb6cd4b1396490bb639f2a6.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+workbox.setConfig({
+  debug: true,
+});
+
+workbox.precaching.precacheAndRoute(self.__precacheManifest);
+
+workbox.routing.registerRoute(
+  new RegExp("images/*"),
+  new workbox.strategies.CacheFirst({
+    cacheName: "public-images",
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 20,
+        maxAgeSeconds: 365 * 24 * 60 * 60,
+      }),
+    ],
+  })
+);
+
+workbox.routing.registerRoute(
+  new RegExp(`(.*?)\.(woff)$`, "i"),
+  new workbox.strategies.CacheFirst({
+    cacheName: "cdn-fonts",
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 5,
+        maxAgeSeconds: 365 * 24 * 60 * 60,
+      }),
+    ],
+  })
+);
+
