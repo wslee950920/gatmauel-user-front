38a04125bdb5f0793bc565ce37bf6d6a2b89cdc3
wslee950920 <wslee950920@gmail.com>
2020-10-03 20:39:16 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/gatmauel-user-front/precache-manifest.1b0fe404a0e2b93d9b37c03daa429c6a.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/gatmauel-user-front/precache-manifest.106b7b7e48ed6e1697316b92e5276e1d.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 workbox.setConfig({
   debug: true,
