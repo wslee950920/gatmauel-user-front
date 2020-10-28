7708b277540caa03fd5d36f15d2e7b2b7ef3fb54
wslee950920 <wslee950920@gmail.com>
2020-10-29 04:09:58 +0900

    favicon, icon 보완

--- src/createPage.js
+++ src/createPage.js
@@ -4,15 +4,30 @@ const createPage = (root, tags) => {
           <html lang="ko">
           <head>
             <meta charset="utf-8" />
-            <link rel="icon" href="/images/icons/favicon.ico" />
+            <link rel="icon" href="/favicon.ico" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <meta name="theme-color" content="#000000" />
+            <meta name="theme-color" content="#feffff" />
             <meta name="description" content="갯마을 바지락 칼국수 보쌈 홈페이지" />
             <meta name="author" content="WSL" />
             <meta name="mobile-web-app-capable" content="yes" />
             <meta name="apple-mobile-web-app-capable" content="yes" />
             <meta name="apple-mobile-web-app-title" content="갯마을" />
-            <link rel="apple-touch-icon" href="/images/icons/favicon.ico" />
+            <link rel="apple-touch-icon" href="/favicon.ico" />
+            <link rel="apple-touch-icon" sizes="57x57" href="/favicons/apple-icon-57x57.png">
+            <link rel="apple-touch-icon" sizes="60x60" href="/favicons/apple-icon-60x60.png">
+            <link rel="apple-touch-icon" sizes="72x72" href="/favicons/apple-icon-72x72.png">
+            <link rel="apple-touch-icon" sizes="76x76" href="/favicons/apple-icon-76x76.png">
+            <link rel="apple-touch-icon" sizes="114x114" href="/favicons/apple-icon-114x114.png">
+            <link rel="apple-touch-icon" sizes="120x120" href="/favicons/apple-icon-120x120.png">
+            <link rel="apple-touch-icon" sizes="144x144" href="/favicons/apple-icon-144x144.png">
+            <link rel="apple-touch-icon" sizes="152x152" href="/favicons/apple-icon-152x152.png">
+            <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-icon-180x180.png">
+            <link rel="icon" type="image/png" sizes="192x192"  href="/favicons/android-icon-192x192.png">
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png">
+            <link rel="icon" type="image/png" sizes="96x96" href="/favicons/favicon-96x96.png">
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png">
+            <meta name="msapplication-TileColor" content="#ffffff">
+            <meta name="msapplication-TileImage" content="/favicons/ms-icon-144x144.png">
             <link rel="manifest" href="/manifest.json" />
             <!-- Google Fonts -->
             <link
@@ -39,7 +54,7 @@ const createPage = (root, tags) => {
               rel="stylesheet"
             />
             <style>
-              .root {
+              .ssr-root {
                 visibility: hidden;
               }
 
@@ -74,11 +89,11 @@ const createPage = (root, tags) => {
           </head>
           <body>
             <noscript>You need to enable JavaScript to run this app.</noscript>
-            <div id="root" class="root">
+            <div id="root" class="ssr-root">
               ${root}
             </div>
             <div class="ssr-loading" id="ssr-loading">
-              <img src="/images/icons/logo192.png" alt="Loading..." />
+              <img src="/favicons/favicon-96x96.png" alt="Loading..." />
             </div>
             ${tags.scripts}
             <script>
@@ -86,7 +101,7 @@ const createPage = (root, tags) => {
                 const loading = document.getElementById("ssr-loading");
                 const root = document.getElementById("root");
 
-                root.className -= "root"
+                root.className -= "ssr-root"
                 loading.className += " hidden"; // class "loader hidden"
               });
             </script>
